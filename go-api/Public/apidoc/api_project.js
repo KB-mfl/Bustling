9beaa20ddba0dac538e061fdbe991b22329c4d2d
gongrongyun <1716175849@qq.com>
2019-08-19 22:48:47 +0800

    test ignore

---
+++ go-api/Public/apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Bustling",
+  "version": "1.0.0",
+  "description": "the api-doc of bustling",
+  "title": "Bustling",
+  "url": "http://localhost/",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-08-17T08:50:48.240Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
