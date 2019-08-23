d2818dae013bf59fabab7cb75ceae9713d9273ba
gongrongyun <1716175849@qq.com>
2019-08-23 15:38:13 +0800

    fix bug

--- go-api/Controller/Article/Create.go
+++ go-api/Controller/Article/Create.go
@@ -4,7 +4,6 @@ import (
 	"Bustling/go-api/Boot/Orm"
 	"Bustling/go-api/Model"
 	"github.com/gin-gonic/gin"
-	uuid "github.com/satori/go.uuid"
 )
 
 /**
--- go-api/Controller/Article/UnLike.go
+++ go-api/Controller/Article/UnLike.go
@@ -3,7 +3,6 @@ package Article
 import (
 	"Bustling/go-api/Boot/Orm"
 	"Bustling/go-api/Model"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	uuid "github.com/satori/go.uuid"
 )
